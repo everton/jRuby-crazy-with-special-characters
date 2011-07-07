5334058e9b15756a5a640dfb9bf484ff11d50d11
Everton J. Carpes <everton.carpes@gmail.com>
2011-07-07 00:27:34 -0300

    rails new jruby-sandbox -m http://jruby.org/rails3.rb

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JrubySandbox::Application.config.secret_token = '0ed09e96e25b40c674811befd4b43f376b48d3f5d50e0be762abeb844b5eb8b63967c0875e63e2627f5a30d2c449a0c241293edb1ffae425fb82ebc87bbd675d'
